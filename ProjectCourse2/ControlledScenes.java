7143a2fa113185ec292ab24de0120e5562bb0e7d
Martijn152 <m.dered@hotmail.com>
2018-05-07 18:24:38 +0200

    Working version commit
    This version is working. All the needed files are in there.
    -Added lib folder with all library files

--- ProjectCourse2/ControlledScenes.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-public interface ControlledScenes {
-
-
-    // This method will allow the injection of the Parent Screenpane
-    public void setScreenParent(ScenesController screenController);
-}
